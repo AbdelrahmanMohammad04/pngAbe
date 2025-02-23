45cb649f31648071b72c23500a2b4a45427f5eed
Abdelrahman Mohammad <51163783+AbdelrahmanMohammad04@users.noreply.github.com>
2025-02-23 12:04:39 -0600

    complete cli logic using clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 mod args;
 mod chunk;
 mod chunk_type;
@@ -7,6 +9,16 @@ mod png;
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
+use args::{PngAbeArgs, PngCommands};
+use commands::{decode, encode, print_chunks, remove};
+
 fn main() -> Result<()> {
-    todo!()
+    let cli = PngAbeArgs::parse();
+
+    match cli.command {
+        PngCommands::Encode(args) => encode(args),
+        PngCommands::Decode(args) => decode(args),
+        PngCommands::Remove(args) => remove(args),
+        PngCommands::Print(args) => print_chunks(args),
+    }
 }
